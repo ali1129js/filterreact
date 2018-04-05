f0f812202e786b313f3670d38295c7295a710365
ali1129js <ali1129js@gmail.com>
2018-04-05 21:46:44 +0200

    input value displyed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-05T20:01:08+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-05T20:02:04+02:00
+ * @Last modified time: 2018-04-05T20:56:46+02:00
  */
 import React, { Component } from 'react';
 import Person from './Components/Person';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+  
   render() {
     return (
       <div className="App">
--- src/Components/PersonItem.js
+++ src/Components/PersonItem.js
@@ -2,15 +2,35 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-05T20:42:57+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-05T20:43:49+02:00
+ * @Last modified time: 2018-04-05T21:44:10+02:00
  */
  import React, { Component } from 'react';
 
  class PersonItem extends Component {
+   constructor(){
+     super();
+     this.state = {
+       text: ''
+     }
+     this.handleChange = this.handleChange.bind(this);
+   }
+   handleChange(e) {
+     const text = e.target.value;
+     this.setState({text: text});
+   }
    render() {
      return (
        <div className="PersonItem">
-         <input />
+         <input
+           type="text"
+           value={this.state.text}
+           onChange={this.handleChange}/>
+         <div>
+           <hr />
+           <h3>
+             {this.state.text}
+           </h3>
+         </div>
        </div>
      );
    }
