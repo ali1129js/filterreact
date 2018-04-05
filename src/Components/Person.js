44faeba74b5d0bd8c562ef9f032248041640ba9c
ali1129js <ali1129js@gmail.com>
2018-04-05 20:48:50 +0200

    another components

--- src/Components/Person.js
+++ src/Components/Person.js
@@ -2,15 +2,15 @@
  * @Author: Ali Ismail
  * @Date:   2018-04-05T20:00:01+02:00
  * @Last modified by:   Ali Ismail
- * @Last modified time: 2018-04-05T20:01:04+02:00
+ * @Last modified time: 2018-04-05T20:44:24+02:00
  */
  import React, { Component } from 'react';
-
+ import PersonItem from './PersonItem'
  class Person extends Component {
    render() {
      return (
        <div className="Person">
-         <input />
+         <PersonItem />
        </div>
      );
    }
---
+++ src/Components/PersonItem.js
@@ -0,0 +1,19 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-05T20:42:57+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-05T20:43:49+02:00
+ */
+ import React, { Component } from 'react';
+
+ class PersonItem extends Component {
+   render() {
+     return (
+       <div className="PersonItem">
+         <input />
+       </div>
+     );
+   }
+ }
+
+ export default PersonItem;
